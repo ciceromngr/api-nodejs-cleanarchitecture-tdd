f6797819624aeba7315d2edf8549ad32d9c914ef
Cicero Romao <cicerotransformice@gmail.com>
2022-01-11 09:42:43 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,4 +1,5 @@
 const MissingParamError = require("../helpers/missing-param-erros")
+const UnauthorizedError = require("../helpers/unauthorized-error")
 const LoginRouter = require("./login-router")
 
 class AuthUseCaseSpy {
@@ -78,5 +79,6 @@ describe('Login Router', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(401)
+        expect(httpResponse.body).toEqual(new UnauthorizedError())
     })
 })
