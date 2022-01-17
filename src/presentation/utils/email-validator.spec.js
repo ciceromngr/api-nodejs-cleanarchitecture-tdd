5bd2297144240e4b5fc58bbf58121b3a673c7afe
Cicero Romao <cicerotransformice@gmail.com>
2022-01-17 07:44:25 -0300

    ensure EmailValidator calls validator with correct email

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -21,7 +21,13 @@ describe('Email Validator', () => {
     it('should return false if validate returns false', () => {
         validator.isEmailValid = false
         const { sut } = makeSut()
-        const isEmailValid = sut.isValid('invalid_emailmail.com')
+        const isEmailValid = sut.isValid('invalid_email@mail.com')
         expect(isEmailValid).toBe(false)
+    })
+
+    it('should call validator with correct email', () => {
+        const { sut } = makeSut()
+        sut.isValid('any_email@mail.com')
+        expect(validator.email).toBe('any_email@mail.com')
     })
 })
