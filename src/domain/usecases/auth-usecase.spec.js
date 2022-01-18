5cf98419b1d661ad3159dbaec2e12a397a62bd2d
Cicero Romao <cicerotransformice@gmail.com>
2022-01-18 14:04:18 -0300

    ensure AuthUseCase thorws if no password to provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,12 @@
 const { MissingParamError } = require("../../utils/errors")
 
 class AuthUseCase {
-    async auth(email) {
+    async auth(email, password) {
         if (!email) {
             throw new MissingParamError('email')
+        }
+        if (!password) {
+            throw new MissingParamError('password')
         }
     }
 }
@@ -13,5 +16,11 @@ describe('Auth UseCase', () => {
         const sut = new AuthUseCase()
         const promisse = sut.auth()
         expect(promisse).rejects.toThrow(new MissingParamError('email'))
+    })
+
+    it('should thorws if no password to provided', () => {
+        const sut = new AuthUseCase()
+        const promisse = sut.auth('any_email@mail.com')
+        expect(promisse).rejects.toThrow(new MissingParamError('password'))
     })
 })
