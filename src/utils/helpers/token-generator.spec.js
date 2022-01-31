c1e381e49567a8abe9bd50615861c059e4c88c17
Cicero Romao <cicerotransformice@gmail.com>
2022-01-31 06:18:09 -0300

    move sut criation to a helper method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -6,22 +6,27 @@ class TokenGenerator {
     }
 }
 
+const makeSut = () => {
+    const sut = new TokenGenerator()
+    return { sut }
+}
+
 describe('TokenGenerator', () => {
     it('should return null if JWT returns null', async() => {
-        const sut = new TokenGenerator()
+        const { sut } = makeSut()
         jwt.token = null
         const token = await sut.generator('any_id')
         expect(token).toBeNull()
     })
 
     it('should return a token if JWT returns token', async() => {
-        const sut = new TokenGenerator()
+        const { sut } = makeSut()
         const token = await sut.generator('any_id')
         expect(token).toBe(jwt.token)
     })
 
     it('should call JWT with correct values', async() => {
-        const sut = new TokenGenerator()
+        const { sut } = makeSut()
         await sut.generator('any_id')
         expect(jwt.id).toBe('any_id')
     })
