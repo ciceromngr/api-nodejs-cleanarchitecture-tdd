578a6aa0f4a669f619be6bf4d442b41432057925
Cicero Romao <cicerotransformice@gmail.com>
2022-02-03 06:46:32 -0300

    ensure requests return xml content-type if forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -11,4 +11,15 @@ describe('Content type Middleware', () => {
             .get('/api/content_type')
             .expect('content-type', /json/)
     })
+
+    it('should return xml content-type if forced', async() => {
+        app.get('/api/content_type_xml', (req, res) => {
+            res.type('xml')
+            res.send('')
+        })
+
+        await request(app)
+            .get('/api/content_type_xml')
+            .expect('content-type', /xml/)
+    })
 })
